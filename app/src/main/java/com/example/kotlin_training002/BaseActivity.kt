8a84fead60e2ee5686a6fb37cb228e6ee705c04f
y-uchiida <y.uchiida@nirneege.com>
2024-01-29 17:37:55 +0900

    メニュー項目クリックによる画面遷移の共通化のため、BaseActivityクラスを作成

---
+++ app/src/main/java/com/example/kotlin_training002/BaseActivity.kt
@@ -0,0 +1,36 @@
+package com.example.kotlin_training002
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+// アクティビティの共通クラス
+open class BaseActivity: AppCompatActivity() {
+
+    // メニューのインフレート
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Log.d("BaseActivity", "onCreateOptionsMenu on BaseActivity")
+        menuInflater.inflate(R.menu.my_app_menu, menu)
+        return true
+    }
+
+    // メニュー項目がクリックされたときの動作
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // クリックされたメニュー項目のIDによって、遷移先のアクティビティを切り替える
+        return when (item.itemId) {
+            R.id.menuItemMain -> {
+                startActivity(Intent(this, MainActivity::class.java))
+                true
+            }
+            R.id.menuItemMyCalendar -> {
+                startActivity(Intent(this, MyCalendarActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
