73785d79739670a50ac26060f9ee17b695fb4e98
y-uchiida <y.uchiida@nirneege.com>
2024-01-29 18:59:21 +0900

    HiAndLowGameのクリア画面のアクティビティを追加

---
+++ app/src/main/java/com/example/kotlin_training002/HighAndLowGameClearActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_training002
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HighAndLowGameClearActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_high_and_low_game_clear)
+    }
+}
