648eed29c6de31caa5c15f0ad6a68520f453d377
y-uchiida <y.uchiida@nirneege.com>
2024-01-29 11:52:32 +0900

    MainActivityからMyCalenderActivityに画面遷移できるようにボタンを設置

--- app/src/main/java/com/example/kotlin_training002/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_training002/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kotlin_training002
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ボタン要素を取得
+        val btnOpenMyCalendarActivity: Button = findViewById(R.id.btnOpenMyCalendar)
+
+        // クリックイベントのリスナーを設定
+        btnOpenMyCalendarActivity.setOnClickListener{
+            val intent = Intent(this, MyCalendarActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
