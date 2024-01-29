1f938ac32ef13b637432656a3d2cd96222d6c087
y-uchiida <y.uchiida@nirneege.com>
2024-01-29 14:34:55 +0900

    MessageDisplayActivity で、受け取ったメッセージを画面に表示する実装を追加

--- app/src/main/java/com/example/kotlin_training002/MessageDisplayActivity.kt
+++ app/src/main/java/com/example/kotlin_training002/MessageDisplayActivity.kt
@@ -11,6 +11,14 @@ class MessageDisplayActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message_display)
 
+        // 送信されたメッセージを取得
+        val message = intent.getStringExtra("message")
+
+        // メッセージを表示するためのText ビューを取得
+        val textView: TextView = findViewById(R.id.sentMessageFromMain)
+        // 表示するテキストを、受け取ったメッセージで代入
+        textView.text = message
+
         val btn: Button = findViewById(R.id.btnOpenMainOnMessageDisplay)
 
         btn.setOnClickListener {
