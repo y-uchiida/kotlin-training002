d4545bce2cd0d09ac79833212e6b1723fbdc4643
y-uchiida <y.uchiida@nirneege.com>
2024-01-31 02:38:11 +0900

    API通信を行うための設定を追加 - HTTP通信用のライブラリ Volley を追加 - インターネット通信の権限要求を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.android.volley:volley:1.2.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     testImplementation("junit:junit:4.13.2")
