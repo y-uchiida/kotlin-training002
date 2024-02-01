e3b969557064b0106d6b57c3d21a6ee5050c361d
y-uchiida <y.uchiida@nirneege.com>
2024-02-02 07:11:42 +0900

    User エンティティに Parcelable を実装 アノテーションを追加するのみでOK

--- app/src/main/java/com/example/kotlin_training002/Entities/User.kt
+++ app/src/main/java/com/example/kotlin_training002/Entities/User.kt
@@ -1,12 +1,17 @@
 package com.example.kotlin_training002.Entities
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 
 // APIから取得されるユーザー情報のエンティティ
+// ユーザー詳細画面のViewに渡す必要があるので、Parcelize インターフェースを実装しておく
+// 実装は @Parcelable アノテーションが済ませてくれる
+@Parcelize
 data class User (
     var id: Long? = null,
     var name: String? = null,
     var email: String? = null,
     var password: String? = null,
     var registered_at: LocalDate? = null,
-)
+): Parcelable
