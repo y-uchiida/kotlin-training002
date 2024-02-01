5b13694ba939e9a4819b3e7b4dd0b79380379b54
y-uchiida <y.uchiida@nirneege.com>
2024-02-02 07:37:12 +0900

    ユーザー詳細画面のViewに、一覧画面で受け取ったUserの内容をセットする

--- app/src/main/java/com/example/kotlin_training002/UserDetailFragment.kt
+++ app/src/main/java/com/example/kotlin_training002/UserDetailFragment.kt
@@ -1,11 +1,13 @@
 package com.example.kotlin_training002
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.kotlin_training002.databinding.FragmentUserDetailBinding
 
 /**
@@ -19,12 +21,22 @@ class UserDetailFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    // ユーザー一覧画面から受け取ったUser オブジェクトを取得するため、navArgs() でりげーどを用いてargs を設定
+    private val args: UserDetailFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentUserDetailBinding.inflate(inflater, container, false)
+
+        val user = args.user
+
+        // ユーザー情報をそれぞれTextViewに設定
+        binding.userNameTextEdit.setText(user.name ?: "")
+        binding.UserEmailText.text = user.email
+        binding.userRegisteredAt.text = user.registered_at.toString()
+
         return binding.root
 
     }
